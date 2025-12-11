45418ace0c495a79967ee7d8eaba118eab21de82
Victor Diogo <victordiogo2801@gmail.com>
2025-12-11 18:10:52 -0300

    atividade 1 modulo 2

--- atividade_1.js
+++ atividade_1.js
@@ -1,11 +1,16 @@
 process.stdout.write("Digite seu nome: ");
 
-process.stdin.on('data', (data) => {
+process.stdin.on('data', function pegarNome(data) {
     let nome = data.toString().trim();
+
+    process.stdin.removeListener('data', pegarNome); 
     process.stdout.write("Digite sua idade:  ")
-    process.stdin.on('data', (data) => {
+
+    process.stdin.on('data', function pegarIdade(data) {
         let idade = data.toString().trim();
-        process.stdout.write("Seu nome é " + nome + ' e sua idade é ' + idade + "anos.");
+
+        process.stdin.removeListener("data", pegarIdade)
+        process.stdout.write("Seu nome é " + nome + ' e sua idade é ' + idade + " anos.");
         process.exit()
-    })
+    });
 });
