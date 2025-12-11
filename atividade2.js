4c0ef8ed02e7f7609a67d0197f25684e7c9eac68
Victor Diogo <victordiogo2801@gmail.com>
2025-12-11 17:34:45 -0300

    Create atividade2.js

---
+++ atividade2.js
@@ -0,0 +1,11 @@
+process.stdout.write("Digite seu nome: ");
+
+process.stdin.on('data', (data) => {
+    let nome = data.toString().trim();
+    process.stdout.write("Digite sua idade:  ")
+    process.stdin.on('data', (data) => {
+        let idade = data.toString().trim();
+        process.stdout.write("Seu nome é " + nome + ' e sua idade é ' + idade + "anos.");
+        process.exit()
+    })
+});
